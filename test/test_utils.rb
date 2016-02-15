jekyll/jekyll

commit 67f842546efa980146778c85fb613e6c9b53dcb4

Fix #4191: Reduce Document#post_read complexity slightly.

lib/jekyll/utils.rb
@@ -10,8 +10,12 @@ module Jekyll
     SLUGIFY_DEFAULT_REGEXP = Regexp.new('[^[:alnum:]]+').freeze
     SLUGIFY_PRETTY_REGEXP = Regexp.new("[^[:alnum:]._~!$&'()+,;=@]+").freeze
 
-    def strip_heredoc(str)
-      str.gsub(/^[ \t]{#{(str.scan(/^[ \t]*(?=\S)/).min || "").size}}/, "")
+    # Takes a slug and turns it into a simple title.
+
+    def titleize_slug(slug)
+      slug.split("-").map! do |val|
+        val.capitalize!
+      end.join(" ")
     end
 
     # Non-destructive version of deep_merge_hashes! See that method.

lib/jekyll/document.rb
@@ -289,26 +289,23 @@ module Jekyll
     end
 
     def post_read
-      if DATE_FILENAME_MATCHER =~ relative_path
-        _, _, date, slug, ext = *relative_path.match(DATE_FILENAME_MATCHER)
-        merge_data!({
-          "slug" => slug,
-          "ext"  => ext
-        }, source: "filename")
-        data['title'] ||= slug.split('-').select(&:capitalize).join(' ')
-        if data['date'].nil? || data['date'].to_i == site.time.to_i
+      if relative_path =~ DATE_FILENAME_MATCHER
+        _, date, slug, ext = $1, $2, $3, $4
+        if !data['date'] || data['date'].to_i == site.time.to_i
           merge_data!({"date" => date}, source: "filename")
         end
-      elsif DATELESS_FILENAME_MATCHER =~ relative_path
-        m, cats, slug, ext = *relative_path.match(DATELESS_FILENAME_MATCHER)
-        data['title'] ||= slug.split('-').select {|w| w.capitalize! || w }.join(' ')
+      elsif relative_path =~ DATELESS_FILENAME_MATCHER
+        _, slug, ext = $1, $2, $3
       end
+
+      merge_data!({"slug" => slug, "ext" => ext}, source: "filename")
+
+      # Try to ensure the user gets a title.
+      data["title"] ||= Utils.titleize_slug(slug)
+
       populate_categories
       populate_tags
-
-      if generate_excerpt?
-        data['excerpt'] ||= Jekyll::Excerpt.new(self)
-      end
+      generate_excerpt
     end
 
     # Add superdirectories of the special_dir to categories.
@@ -443,6 +440,13 @@ module Jekyll
         data[method.to_s]
       else
         super
+      end
+    end
+
+    private # :nodoc:
+    def generate_excerpt
+      if generate_excerpt?
+        data["excerpt"] ||= Jekyll::Excerpt.new(self)
       end
     end
   end

favdev111/icomoon-docker

commit 1e80ce5ec048d5d72c81944ea003d3ac4fb41f5d

Fix titleize_slug so already capitalized words are not dropped
Previously `titleize` used `capitalize!` which has the side effect of
returning `nil` for anything already starting with a capital letter. This
commit changes it to just `capitalize`.

Example, before:

A file "2016-01-01-This-is-a-title-with-Capitals.markdown" would return "Is A
Title With" for `post.title`

Example, after:

A file "2016-01-01-This-is-a-title-with-Capitals.markdown" will return "This Is A
Title With Capitals" for `post.title`

Tests added for `titleize_slug` in test_utils.rb

Fix problem introduced in 67f842546efa980146778c85fb613e6c9b53dcb4

References #4525

lib/jekyll/utils.rb
@@ -20,7 +20,7 @@ module Jekyll
 
     def titleize_slug(slug)
       slug.split("-").map! do |val|
-        val.capitalize!
+        val.capitalize
       end.join(" ")
     end
 

test/test_utils.rb
@@ -206,6 +206,14 @@ class TestUtils < JekyllUnitTest
     end
   end
 
+  context "The \`Utils.titleize_slug\` method" do
+    should "capitalize all words and not drop any words" do
+      assert_equal "This Is A Long Title With Mixed Capitalization", Utils.titleize_slug("This-is-a-Long-title-with-Mixed-capitalization")
+      assert_equal "This Is A Title With Just The Initial Word Capitalized", Utils.titleize_slug("This-is-a-title-with-just-the-initial-word-capitalized")
+      assert_equal "This Is A Title With No Capitalization", Utils.titleize_slug("this-is-a-title-with-no-capitalization")
+    end
+  end
+
   context "The \`Utils.add_permalink_suffix\` method" do
     should "handle built-in permalink styles" do
       assert_equal "/:basename/", Utils.add_permalink_suffix("/:basename", :pretty)